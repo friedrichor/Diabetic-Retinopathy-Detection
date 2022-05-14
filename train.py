e9e43d8861aa66c3bfc784c1fc95f108d094b755
friedrichor <kongfanheng426@163.com>
2022-05-14 15:57:58 +0800

    Update train.py

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def main(args):
         tb_writer.add_scalar(tags[3], val_acc, epoch)
         tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
 
-        torch.save(model.state_dict(), (ROOT / 'weights/model-{}.pth').format(epoch))
+        torch.save(model.state_dict(), str(ROOT / 'weights/model-{}.pth').format(epoch))
 
 
 if __name__ == '__main__':
